80cabe18a634cd8628d8caba2403a944f91c903b
RaeStriker <98552320+RaeStriker@users.noreply.github.com>
2022-02-17 23:27:47 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace randRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public int Length { get; }
+        public int Width { get; }
+    }
+
+    public class Utility
+    {
+        public static int calcArea(int length, int width)
+        {
+            int area = length * width;
+            return area;
+        }
+        public static int calcPerimeter(int length, int width)
+        {
+            int perimeter = 2 * (length + width);
+            return perimeter;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
